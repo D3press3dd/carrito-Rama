7dcbbe66ecf6df22bfb7d67e5fce82f84b0579b2
D3press3d <anthonyrosman20@gmail.com>
2024-09-11 09:09:04 -0400

    fix: no reasigna solo muta el length, un truco sucio pero valido

--- js/VaciarCarrito.js
+++ js/VaciarCarrito.js
@@ -7,8 +7,9 @@ function vaciarCarrito() {
     if (confirm('¿Está seguro que desea vaciar el carrito?')) {
       overlayContainer.innerHTML = 'Carrito Vacio ☹️';
       containerTotal.style.display = 'none';
-      productosActualizados = [];
-      carritoID = []; 
+      // cuando haces importacion de una variable javascript la trata como inmutable por eso no puedes reasignar directamente, pero si puedes cambiar el tamaño para que sea cero o tambien puedes crear una funcion que mute tu array y exportala directamente (que seria lo mejor), pero me imaginos que estas aprendiendo asi que sigue peleandote con eso, tu puedes!!
+      productosActualizados.length = 0;
+      carritoID.length = 0; 
       localStorage.setItem('Productos-Actualizados', JSON.stringify(productosActualizados));
     
       setTimeout(() => {
